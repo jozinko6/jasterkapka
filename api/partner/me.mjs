953e3316befee35c47c67c5a5c45ceeb785ac784
jozinko6 <37715685+jozinko6@users.noreply.github.com>
2026-06-07 12:09:07 +0200

    Remove explicit partner me route

--- api/partner/me.mjs
+++
@@ -1,8 +0,0 @@
-import { methodNotAllowed } from "../../server/_lib/http.mjs";
-import { GET } from "../../server/partner/me.mjs";
-
-export default {
-  fetch(request) {
-    return request.method === "GET" ? GET(request) : methodNotAllowed();
-  }
-};
