2a91e770914b90f1166c8c81d56eb258047c0d95
jozinko6 <37715685+jozinko6@users.noreply.github.com>
2026-06-07 12:09:03 +0200

    Remove explicit courier earnings route

--- api/courier/earnings.mjs
+++
@@ -1,8 +0,0 @@
-import { methodNotAllowed } from "../../server/_lib/http.mjs";
-import { GET } from "../../server/courier/earnings.mjs";
-
-export default {
-  fetch(request) {
-    return request.method === "GET" ? GET(request) : methodNotAllowed();
-  }
-};
